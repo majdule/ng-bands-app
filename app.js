c8715cb6b3f8b3b8f60173bf5f2d36f9ba5d5d58
MAJ1BG <dusan.majstorovic@bosch.com>
2018-01-30 10:06:31 +0100

    Separate services and controllers

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-var app = angular.module('bands', []);
-
-app.controller('mainController', function ($scope, $http) {
-  $scope.bands = [];
-
-  $http({
-    method : "get",
-    url : ""
-  });
-});
+angular.module('bands', ['mainController','dbService']);
---
+++ controllers/mainController.js
@@ -0,0 +1,16 @@
+angular.module('mainController', [])
+  .controller('mainController', function ($scope, db) {
+    $scope.bands = [];
+    $scope.activeBand = {};
+
+    db.getData().then(function (result) {
+      $scope.bands = result.data;
+    }, function (error) {
+      console.log(error);
+    });
+
+    $scope.display = function (band) {
+      $scope.activeBand = band;
+    }
+
+  });
